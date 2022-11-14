da32f94653bfc46b3daaf4003c2049324148e574
Etienne Ott <etienne.ott@siz-energieplus.de>
2022-11-14 16:04:21 +0100

    Move functionality for loading a project from file to own file.

---
+++ src/project_loading.jl
@@ -0,0 +1,152 @@
+# this file contains functionality pertaining to loading a project's metadata and the
+# energy systems from the project config file, as well as constructing certain helpful
+# information data structures from the inputs in the config
+
+import JSON
+
+"""
+    read_JSON(filepath)
+
+Read and parse the JSON-encoded Dict in the given file.
+"""
+function read_JSON(filepath :: String) :: Dict{AbstractString, Any}
+    open(filepath, "r") do file_handle
+        content = read(file_handle, String)
+        return JSON.parse(content)
+    end
+end
+
+"""
+load_systems(config)
+
+Construct instances of energy systems from the given config.
+
+The config must have the structure:
+```
+{
+"UAC key": {
+    "type": "PVPlant",
+    ...
+},
+...
+}
+```
+
+The required parameters to construct an energy system from one entry in the config must
+match what is required for the particular system. The `type` parameter must be present and
+must match the symbol of the energy system class exactly. The structure is described in
+more detail in the accompanying documentation on the project file.
+"""
+function load_systems(config :: Dict{String, Any}) :: Grouping
+    systems = Grouping()
+
+    for (unit_key, entry) in pairs(config)
+        symbol = Symbol(String(entry["type"]))
+        unit_class = getproperty(EnergySystems, symbol)
+        if unit_class <: EnergySystems.EnergySystem
+            instance = unit_class(unit_key, entry)
+            systems[unit_key] = instance
+        end
+    end
+
+    for (unit_key, entry) in pairs(config)
+        if length(entry["control_refs"]) > 0
+            others = Grouping(key => systems[key] for key in entry["control_refs"])
+            link_control_with(systems[unit_key], others)
+        end
+
+        if length(entry["production_refs"]) > 0
+            others = Grouping(key => systems[key] for key in entry["production_refs"])
+            link_production_with(systems[unit_key], others)
+        end
+    end
+
+    return systems
+end
+
+"""
+order_of_steps(systems)
+
+Calculate the order of steps that need to be performed to simulate the given systems.
+
+This function works by an algorithm described in more detail in the accompanying
+documentation. The result of this are step-by-step instructions telling the simulation
+engine in which order the system functions are performed for each unit. This algorithm is
+not trivial and might not work for each possible grouping of systems.
+
+# Args
+- `system::Grouping`: The systems for which an order is required
+# Returns
+- `StepInstructions`: The order in the structure:
+```
+[
+    ["UAC Key", s_step],
+    ...
+]
+```
+"""
+function order_of_steps(systems :: Grouping) :: StepInstructions
+    systems_by_function = [
+        [unit for unit in each(systems)
+            if unit.sys_function == EnergySystems.sf_fixed_source],
+        [unit for unit in each(systems)
+            if unit.sys_function == EnergySystems.sf_fixed_sink],
+        [unit for unit in each(systems)
+            if unit.sys_function == EnergySystems.sf_bus],
+        [unit for unit in each(systems)
+            if unit.sys_function == EnergySystems.sf_transformer],
+        [unit for unit in each(systems)
+            if unit.sys_function == EnergySystems.sf_storage],
+        [unit for unit in each(systems)
+            if unit.sys_function == EnergySystems.sf_dispatchable_source],
+        [unit for unit in each(systems)
+            if unit.sys_function == EnergySystems.sf_dispatchable_sink],
+    ]
+
+    simulation_order = []
+    initial_nr = length(systems) * 7
+
+    # reset all systems, order doesn't matter
+    for sf_order = 1:7
+        for unit in values(systems_by_function[sf_order])
+            push!(simulation_order, (initial_nr, (unit.uac, EnergySystems.s_reset)))
+            initial_nr -= 1
+        end
+    end
+
+    # control/produce all systems except dispatchable sources/sinks. the order corresponds
+    # to the general order of system functions
+    for sf_order = 1:5
+        for unit in values(systems_by_function[sf_order])
+            push!(simulation_order, (initial_nr, (unit.uac, EnergySystems.s_control)))
+            initial_nr -= 1
+            push!(simulation_order, (initial_nr, (unit.uac, EnergySystems.s_produce)))
+            initial_nr -= 1
+        end
+    end
+
+    # sandwich loading of storage systems between the other systems and dispatchable ones
+    for unit in values(systems_by_function[5])
+        push!(simulation_order, (initial_nr, (unit.uac, EnergySystems.s_load)))
+        initial_nr -= 1
+    end
+
+    # handle dispatchable sources/sinks
+    for sf_order = 6:7
+        for unit in values(systems_by_function[sf_order])
+            push!(simulation_order, (initial_nr, (unit.uac, EnergySystems.s_control)))
+            initial_nr -= 1
+            push!(simulation_order, (initial_nr, (unit.uac, EnergySystems.s_produce)))
+            initial_nr -= 1
+        end
+    end
+
+    # finally, distribute bus systems
+    for unit in values(systems_by_function[3])
+        push!(simulation_order, (initial_nr, (unit.uac, EnergySystems.s_distribute)))
+        initial_nr -= 1
+    end
+
+    fn_first = function(entry) return entry[1] end
+    return [u[2] for u in sort(simulation_order, by=fn_first, rev=true)]
+end
