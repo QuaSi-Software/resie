15a5b64664f6440b9a1fed94816bd70abb60f6b2
Etienne Ott <etienne.ott@siz-energieplus.de>
2022-12-08 14:10:22 +0100

    Add some tests for energy system control functionality.

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,4 +4,7 @@ using Test
     @test true
 end
 
+# there is an order to the includes in the sense that later tests might use functionality
+# that is being tested in earlier test sets, in particular the project loading methods
 include("tests_project_loading.jl")
+include("tests_control.jl")
---
+++ test/tests_control.jl
@@ -0,0 +1,53 @@
+using Bran.EnergySystems
+
+@testset "control_tests" begin
+    systems_config = Dict{String, Any}(
+        "TST_BT_01" => Dict{String, Any}(
+            "type" => "BufferTank",
+            "control_refs" => [],
+            "production_refs" => [],
+            "capacity" => 40000,
+            "load" => 20000,
+            "strategy" => Dict{String, Any}(
+                "name" => "Default"
+            )
+        ),
+        "TST_HP_01" => Dict{String, Any}(
+            "type" => "HeatPump",
+            "control_refs" => ["TST_BT_01"],
+            "production_refs" => [],
+            "strategy" => Dict{String, Any}(
+                "name" => "storage_driven",
+                "high_threshold" => 0.9,
+                "low_threshold" => 0.2
+            ),
+            "power" => 20000,
+            "cop" => 3.0
+        ),
+    )
+    systems = Bran.load_systems(systems_config)
+    buffer_tank = systems["TST_BT_01"]
+    heat_pump = systems["TST_HP_01"]
+
+    simulation_params = Dict{String, Any}(
+        "time" => 0,
+        "time_step_seconds" => 900,
+        "epsilon" => 1e-9
+    )
+
+    @testset "move_state_default_strategy" begin
+        @test buffer_tank.controller.state_machine.state == 1
+        EnergySystems.move_state(buffer_tank, systems, simulation_params)
+        @test buffer_tank.controller.state_machine.state == 1
+    end
+
+    @testset "move_state_storage_driven_hp" begin
+        buffer_tank.load = 20000
+        heat_pump.controller.state_machine.state = 1
+        EnergySystems.move_state(heat_pump, systems, simulation_params)
+        @test heat_pump.controller.state_machine.state == 1
+        buffer_tank.load = 0
+        EnergySystems.move_state(heat_pump, systems, simulation_params)
+        @test heat_pump.controller.state_machine.state == 2
+    end
+end
