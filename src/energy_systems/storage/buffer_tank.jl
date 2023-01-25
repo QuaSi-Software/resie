b6980be43a18c0373bb452bea3a597a72ec75afa
Etienne Ott <etienne.ott@siz-energieplus.de>
2023-01-25 16:40:49 +0100

    Refactor model of BufferTank to include temperatures of input and output.

--- src/energy_systems/storage/buffer_tank.jl
+++ src/energy_systems/storage/buffer_tank.jl
@@ -17,6 +17,9 @@ mutable struct BufferTank <: ControlledSystem
     capacity :: Float64
     load :: Float64
 
+    high_temperature :: Float64
+    low_temperature :: Float64
+
     function BufferTank(uac :: String, config :: Dict{String, Any})
         return new(
             uac, # uac
@@ -31,43 +34,66 @@ mutable struct BufferTank <: ControlledSystem
                 m_h_w_ht1 => nothing
             ),
             config["capacity"], # capacity
-            config["load"] # load
+            config["load"], # load
+            "high_temperature" in keys(config) # high_temperature
+                ? config["high_temperature"]
+                : 75.0,
+            "low_temperature" in keys(config) # high_temperature
+                ? config["low_temperature"]
+                : 20.0
         )
     end
 end
 
+function temperature_at_load(unit :: BufferTank) :: Temperature
+    switch_point = 0.15
+    partial_load = min(1.0, unit.load / (unit.capacity * switch_point))
+    return (unit.high_temperature - unit.low_temperature) * partial_load + unit.low_temperature
+end
+
 function produce(unit :: BufferTank, parameters :: Dict{String, Any}, watt_to_wh :: Function)
     outface = unit.output_interfaces[m_h_w_ht1]
-    balance, _, _ = balance_on(outface, outface.target)
+    balance, _, demand_temp = balance_on(outface, outface.target)
 
     if balance >= 0.0
         return # produce is only concerned with moving energy to the target
     end
 
+    if demand_temp !== nothing && demand_temp > temperature_at_load(unit)
+        return # we can only supply energy if it's at a higher temperature,
+               # effectively reducing the tank's capacity for any demand at
+               # a temperature higher than the lower limit of the tank
+    end
+
     if unit.load > abs(balance)
         unit.load += balance
-        add!(outface, abs(balance))
+        add!(outface, abs(balance), demand_temp)
     else
-        add!(outface, unit.load)
+        add!(outface, unit.load, demand_temp)
         unit.load = 0.0
     end
 end
 
 function load(unit :: BufferTank, parameters :: Dict{String, Any}, watt_to_wh :: Function)
     inface = unit.input_interfaces[m_h_w_ht1]
-    balance, _, _ = balance_on(inface, inface.source)
+    balance, _, supply_temp = balance_on(inface, inface.source)
 
     if balance <= 0.0
         return # load is only concerned with receiving energy from the target
     end
 
+    if supply_temp !== nothing && supply_temp < unit.low_temperature
+        return # we can only take in energy if it's at a higher temperature than the
+               # tank's lower limit
+    end
+
     diff = unit.capacity - unit.load
     if diff > balance
         unit.load += balance
-        sub!(inface, balance)
+        sub!(inface, balance, supply_temp)
     else
         unit.load = unit.capacity
-        sub!(inface, diff)
+        sub!(inface, diff, supply_temp)
     end
 end
 
