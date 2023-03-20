93dd4bcd0463ee706ccce6e2e490c9d31747150b
Etienne Ott <etienne.ott@siz-energieplus.de>
2023-03-20 12:49:31 +0100

    Add utility functions for performing tests as includeable file.

---
+++ test/test_util.jl
@@ -0,0 +1,43 @@
+"""
+    pwc_units_astr(expected, actual)
+
+Piece-wise comparison of energy system units noting differences in the return string.
+"""
+function pwc_units_astr(expected::Vector, actual::Vector)
+    max_length = max(length(expected), length(actual))
+    result = ""
+
+    for i in 1:max_length
+        if i > length(expected)
+            result = result * "#$i: nothing != $(actual[i].uac) \n"
+        elseif i > length(actual)
+            result = result * "#$i: $(expected[i].uac) != nothing\n"
+        elseif expected[i].uac != actual[i].uac
+            result = result * "#$i: $(expected[i].uac) != $(actual[i].uac)\n"
+        end
+    end
+
+    return result
+end
+
+"""
+    pwc_steps_astr(expected, actual)
+
+Piece-wise comparison of step instructions noting differences in the return string.
+"""
+function pwc_steps_astr(expected::Vector, actual::Vector)
+    max_length = max(length(expected), length(actual))
+    result = ""
+
+    for i in 1:max_length
+        if i > length(expected)
+            result = result * "#$i: nothing !=  $(actual[i][1])-$(actual[i][2])\n"
+        elseif i > length(actual)
+            result = result * "#$i: $(expected[i][1])-$(expected[i][2]) != nothing\n"
+        elseif expected[i][1] != actual[i][1] || expected[i][2] != actual[i][2]
+            result = result * "#$i: $(expected[i][1])-$(expected[i][2]) != $(actual[i][1])-$(actual[i][2])\n"
+        end
+    end
+
+    return result
+end
