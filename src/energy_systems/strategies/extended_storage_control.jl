ae98460ec45839c07b7f620817b7f30b218c52fb
heiner.steinacker <heiner.steinacker@siz-energieplus.de>
2023-03-28 13:17:16 +0200

    adapted description of extended_storage_control strategy

--- src/energy_systems/strategies/extended_storage_control.jl
+++ src/energy_systems/strategies/extended_storage_control.jl
@@ -12,6 +12,12 @@ NOTE: If the properties of load_any_storage and unload_any_storage of different 
       fit together, unexpected results can be retrieved. If a storage is allowed to load all other storages, 
       the restrictions of the other storages are ignored. If one of them is set to "unload_any_storage=false", 
       this can not be garanteed as is highly depends on the order of operation.
+      To avoid this issue, it is recommended to only use the flag "load_any_storage" and leave "unload_any_storage"
+      by it's default value. "load_any_storage" affects the produce() step of the storage and if set to true, 
+      the storage potential is added to the energy that should be provided. This energy is then written to the balance
+      of the interface, so the load() step of storages downstream of the first storage can not detect, from where the
+      energy came from. The loading of only a selection of downstream storages from one upstream storage can not be 
+      modeled reliably as this depends on the order of operation of the produce() and load() steps of all storages.
 """
 
 OP_STRATS["extended_storage_control"] = OperationalStrategyType(
