58da04451d5a4de4db33eca8675fc0e797d6071f
Etienne Ott <etienne.ott@siz-energieplus.de>
2023-09-29 11:26:01 +0200

    Add test case for gas boiler with PLR-dependant efficiency.

--- test/tests_energy_systems.jl
+++ test/tests_energy_systems.jl
@@ -9,4 +9,5 @@ using Test
     include("energy_systems/multiple_transformer_limited.jl")
     include("energy_systems/gasboiler_demand_driven_with_bus.jl")
     include("energy_systems/extended_storage_control_strategy.jl")
+    include("energy_systems/gas_boiler_plr_efficiency.jl")
 end
