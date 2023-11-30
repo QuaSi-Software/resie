6864188b534d7df6e47be32082e2cbc0f6fe5841
Etienne Ott <etienne.ott@siz-energieplus.de>
2023-11-30 10:50:47 +0100

    Add test cases for balance and distribution on a bus with storages involved.

---
+++ test/energy_systems/balance_and_distribution/one_plus_storage_to_many.jl
@@ -0,0 +1,212 @@
+using Debugger
+using Test
+using Resie
+using Resie.EnergySystems
+using Resie.Profiles
+
+EnergySystems.set_timestep(900)
+
+function test_one_plus_storage_to_many()
+    components_config = Dict{String,Any}(
+        "TST_SRC_01" => Dict{String,Any}(
+            "type" => "BoundedSupply",
+            "medium" => "m_h_w_ht1",
+            "control_refs" => [],
+            "strategy" => Dict{String,Any}(
+                "name" => "extended_storage_control",
+                "load_any_storage" => true,
+            ),
+            "output_refs" => ["TST_BUS_TH_01"],
+            "static_power" => 4000,
+            "static_temperature" => 55,
+        ),
+        "TST_DEM_01" => Dict{String,Any}(
+            "type" => "Demand",
+            "medium" => "m_h_w_ht1",
+            "control_refs" => [],
+            "output_refs" => [],
+            "static_load" => 500,
+            "static_temperature" => 55,
+        ),
+        "TST_DEM_02" => Dict{String,Any}(
+            "type" => "Demand",
+            "medium" => "m_h_w_ht1",
+            "control_refs" => [],
+            "output_refs" => [],
+            "static_load" => 500,
+            "static_temperature" => 55,
+        ),
+        "TST_TES_01" => Dict{String,Any}(
+            "type" => "Storage",
+            "medium" => "m_h_w_ht1",
+            "control_refs" => [],
+            "output_refs" => ["TST_BUS_TH_01"],
+            "capacity" => 10000,
+            "load" => 5000,
+        ),
+        "TST_BUS_TH_01" => Dict{String,Any}(
+            "type" => "Bus",
+            "medium" => "m_h_w_ht1",
+            "control_refs" => [],
+            "output_refs" => [
+                "TST_DEM_01",
+                "TST_DEM_02",
+                "TST_TES_01",
+            ],
+            "connection_matrix" => Dict{String,Any}(
+                "input_order" => [
+                    "TST_SRC_01",
+                    "TST_TES_01",
+                ],
+                "output_order" => [
+                    "TST_DEM_01",
+                    "TST_DEM_02",
+                    "TST_TES_01",
+                ],
+                "storage_loading" => [
+                    [1,1,1],
+                    [1,1,0],
+                ],
+            ),
+        ),
+    )
+    components = Resie.load_components(components_config)
+    source = components["TST_SRC_01"]
+    demand_1 = components["TST_DEM_01"]
+    demand_2 = components["TST_DEM_02"]
+    bus = components["TST_BUS_TH_01"]
+    storage = components["TST_TES_01"]
+
+    simulation_parameters = Dict{String,Any}(
+        "time_step_seconds" => 900,
+        "time" => 0,
+    )
+
+    # time step 1: the source can't cover the demands, the storage needs to fill the rest
+
+    source.static_power = 3000
+
+    EnergySystems.reset(demand_1)
+    EnergySystems.reset(demand_2)
+    EnergySystems.reset(bus)
+    EnergySystems.reset(storage)
+    EnergySystems.reset(source)
+
+    EnergySystems.control(demand_1, components, simulation_parameters)
+    EnergySystems.control(demand_2, components, simulation_parameters)
+    EnergySystems.control(bus, components, simulation_parameters)
+    EnergySystems.control(storage, components, simulation_parameters)
+    EnergySystems.control(source, components, simulation_parameters)
+
+    EnergySystems.process(demand_1, simulation_parameters)
+    @test demand_1.input_interfaces[demand_1.medium].balance == -500.0
+    @test demand_1.input_interfaces[demand_1.medium].temperature == 55.0
+
+    EnergySystems.process(demand_2, simulation_parameters)
+    @test demand_2.input_interfaces[demand_2.medium].balance == -500.0
+    @test demand_2.input_interfaces[demand_2.medium].temperature == 55.0
+
+    EnergySystems.process(bus, simulation_parameters)
+
+    EnergySystems.process(source, simulation_parameters)
+    @test source.output_interfaces[source.medium].balance == 750.0
+    @test source.output_interfaces[source.medium].temperature == 55.0
+
+    EnergySystems.process(storage, simulation_parameters)
+    @test storage.output_interfaces[storage.medium].balance == 250.0
+    EnergySystems.load(storage, simulation_parameters)
+    @test storage.input_interfaces[storage.medium].balance == 0.0
+
+    blnc = EnergySystems.balance(bus)
+    @test blnc == 0.0
+
+    EnergySystems.distribute!(bus)
+    @test storage.load == 4750
+
+    # time step 2: the source covers the demands exactly, the storage does nothing
+
+    source.static_power = 4000
+
+    EnergySystems.reset(demand_1)
+    EnergySystems.reset(demand_2)
+    EnergySystems.reset(bus)
+    EnergySystems.reset(storage)
+    EnergySystems.reset(source)
+
+    EnergySystems.control(demand_1, components, simulation_parameters)
+    EnergySystems.control(demand_2, components, simulation_parameters)
+    EnergySystems.control(bus, components, simulation_parameters)
+    EnergySystems.control(storage, components, simulation_parameters)
+    EnergySystems.control(source, components, simulation_parameters)
+
+    EnergySystems.process(demand_1, simulation_parameters)
+    @test demand_1.input_interfaces[demand_1.medium].balance == -500.0
+    @test demand_1.input_interfaces[demand_1.medium].temperature == 55.0
+
+    EnergySystems.process(demand_2, simulation_parameters)
+    @test demand_2.input_interfaces[demand_2.medium].balance == -500.0
+    @test demand_2.input_interfaces[demand_2.medium].temperature == 55.0
+
+    EnergySystems.process(bus, simulation_parameters)
+
+    EnergySystems.process(source, simulation_parameters)
+    @test source.output_interfaces[source.medium].balance == 1000.0
+    @test source.output_interfaces[source.medium].temperature == 55.0
+
+    EnergySystems.process(storage, simulation_parameters)
+    @test storage.output_interfaces[storage.medium].balance == 0.0
+    EnergySystems.load(storage, simulation_parameters)
+    @test storage.input_interfaces[storage.medium].balance == 0.0
+
+    blnc = EnergySystems.balance(bus)
+    @test blnc == 0.0
+
+    EnergySystems.distribute!(bus)
+    @test storage.load == 4750
+
+    # time step 3: the source can cover the demands and has excess production capacity to
+    # load the storage
+
+    source.static_power = 5000
+
+    EnergySystems.reset(demand_1)
+    EnergySystems.reset(demand_2)
+    EnergySystems.reset(bus)
+    EnergySystems.reset(storage)
+    EnergySystems.reset(source)
+
+    EnergySystems.control(demand_1, components, simulation_parameters)
+    EnergySystems.control(demand_2, components, simulation_parameters)
+    EnergySystems.control(bus, components, simulation_parameters)
+    EnergySystems.control(storage, components, simulation_parameters)
+    EnergySystems.control(source, components, simulation_parameters)
+
+    EnergySystems.process(demand_1, simulation_parameters)
+    @test demand_1.input_interfaces[demand_1.medium].balance == -500.0
+    @test demand_1.input_interfaces[demand_1.medium].temperature == 55.0
+
+    EnergySystems.process(demand_2, simulation_parameters)
+    @test demand_2.input_interfaces[demand_2.medium].balance == -500.0
+    @test demand_2.input_interfaces[demand_2.medium].temperature == 55.0
+
+    EnergySystems.process(bus, simulation_parameters)
+
+    EnergySystems.process(source, simulation_parameters)
+    @test source.output_interfaces[source.medium].balance == 1250.0
+    @test source.output_interfaces[source.medium].temperature == 55.0
+
+    EnergySystems.process(storage, simulation_parameters)
+    @test storage.output_interfaces[storage.medium].balance == 0.0
+    EnergySystems.load(storage, simulation_parameters)
+    @test storage.input_interfaces[storage.medium].balance == -250.0
+
+    blnc = EnergySystems.balance(bus)
+    @test blnc == 0.0
+
+    EnergySystems.distribute!(bus)
+    @test storage.load == 5000
+end
+
+@testset "one_plus_storage_to_many" begin
+    test_one_plus_storage_to_many()
+end
