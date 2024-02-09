1877131eaca947f26bb07ff322070bc5140b72de
Etienne Ott <etienne.ott@siz-energieplus.de>
2024-02-09 14:58:08 +0100

    Implement algorithm for automatically merging busses by a graph-based algorithm.

--- src/energy_systems/connections/bus_merging.jl
+++ src/energy_systems/connections/bus_merging.jl
@@ -88,3 +88,195 @@ function merge(first::Bus, second::Bus, uac::String)::Bus
 
     return new_bus
 end
+
+Base.@kwdef mutable struct BusNode
+    uac::String
+    inputs::Vector{BusNode}
+    outputs::Vector{BusNode}
+end
+
+function is_linear_connection(parent::BusNode, child::BusNode)::Bool
+    nr_children = length(parent.outputs)
+    nr_parents = length(child.inputs)
+    return nr_children == 1 && nr_parents == 1
+end
+
+function find_next_linear_connection(
+    chain::Dict{String,BusNode}
+)::Tuple{Union{Nothing,BusNode},Union{Nothing,BusNode}}
+    for bus in values(chain)
+        for child in bus.outputs
+            if is_linear_connection(bus, child)
+                return (bus, child)
+            end
+        end
+        for parent in bus.inputs
+            if is_linear_connection(parent, bus)
+                return (parent, bus)
+            end
+        end
+    end
+    return (nothing, nothing)
+end
+
+function nodes_from_components(components::Grouping)::Dict{String,BusNode}
+    nodes = Dict{String,BusNode}()
+    for uac in keys(components)
+        nodes[uac] = BusNode(
+            uac=uac,
+            inputs=Vector{BusNode}(),
+            outputs=Vector{BusNode}()
+        )
+    end
+    for (uac, bus) in pairs(components)
+        node = nodes[uac]
+        node.inputs = [nodes[i.source.uac] for i in filter_inputs(bus, sf_bus, true)]
+        node.outputs = [nodes[o.target.uac] for o in filter_outputs(bus, sf_bus, true)]
+    end
+    return nodes
+end
+
+function merge(parent::BusNode, child::BusNode, new_uac::String)::BusNode
+    new_node = BusNode(
+        uac=new_uac,
+        inputs=[],
+        outputs=[]
+    )
+    append!(new_node.inputs, parent.inputs)
+    append!(new_node.inputs, [n for n in child.inputs if n.uac != parent.uac])
+    append!(new_node.outputs, child.outputs)
+    append!(new_node.outputs, [n for n in parent.outputs if n.uac != child.uac])
+    return new_node
+end
+
+function update_nodes!(
+    nodes::Dict{String,BusNode},
+    parent_uac::String,
+    child_uac::String,
+    new_node::BusNode
+)
+    for node in values(nodes)
+        for (idx,output) in pairs(node.outputs)
+            if output.uac == parent_uac || output.uac == child_uac
+                splice!(node.outputs, idx, [new_node])
+            end
+        end
+        for (idx,input) in pairs(node.inputs)
+            if input.uac == parent_uac || input.uac == child_uac
+                splice!(node.inputs, idx, [new_node])
+            end
+        end
+    end
+end
+
+function priority_of_child(parent::BusNode, child::BusNode)::Integer
+    indices = indexin([child], parent.outputs)
+    return indices[1] === nothing ? 0 : indices[1]
+end
+
+function priority_of_parent(parent::BusNode, child::BusNode)::Integer
+    indices = indexin([parent], child.inputs)
+    return indices[1] === nothing ? 0 : indices[1]
+end
+
+function find_single_parent_leaves(
+    nodes::Dict{String,BusNode}
+)::Vector{Tuple{Integer,BusNode,BusNode}}
+    leaves = []
+    for node in values(nodes)
+        if length(node.inputs) != 1 || length(node.outputs) > 0
+            continue
+        end
+        parent = node.inputs[1]
+        push!(leaves, (priority_of_child(parent, node), parent, node))
+    end
+    return leaves
+end
+
+function find_parents_of_leaves(
+    nodes::Dict{String,BusNode}
+)::Vector{Tuple{Integer,BusNode,BusNode}}
+    parents = []
+    for node in values(nodes)
+        if length(node.outputs) > 0
+            continue
+        end
+        for parent in node.inputs
+            push!(parents, (priority_of_parent(parent, node), parent, node))
+        end
+    end
+    return parents
+end
+
+function merge_busses(busses_to_merge::Grouping)::Union{Nothing,Bus}
+    nodes = nodes_from_components(busses_to_merge)
+    components = Grouping()
+    for (key,val) in pairs(busses_to_merge)
+        components[key] = val
+    end
+
+    # first remove all linear connections
+    parent, child = find_next_linear_connection(nodes)
+    while parent !== nothing
+        new_uac = parent.uac * child.uac
+        new_bus = merge(components[parent.uac], components[child.uac], new_uac)
+        components[new_uac] = new_bus
+
+        new_node = merge(parent, child, new_uac)
+        nodes[new_uac] = new_node
+        update_nodes!(nodes, parent.uac, child.uac, new_node)
+        delete!(nodes, parent.uac)
+        delete!(nodes, child.uac)
+
+        parent, child = find_next_linear_connection(nodes)
+    end
+
+    # now keep merging until we have only one node left
+    while length(nodes) > 1
+        # prioritise leaves with only one parent and order by descending priority
+        leaves = find_single_parent_leaves(nodes)
+        if length(leaves) > 0
+            next = sort(leaves, by=x->x[1], rev=true)[1]
+            parent = next[2]
+            child = next[3]
+
+            new_uac = parent.uac * child.uac
+            new_bus = merge(components[parent.uac], components[child.uac], new_uac)
+            components[new_uac] = new_bus
+
+            new_node = merge(parent, child, new_uac)
+            nodes[new_uac] = new_node
+            update_nodes!(nodes, parent.uac, child.uac, new_node)
+            delete!(nodes, parent.uac)
+            delete!(nodes, child.uac)
+
+            continue
+        end
+
+        # for leaves with multiple parents, merge into those with the last priority
+        parents = find_parents_of_leaves(nodes)
+        if length(parents) > 0
+            next = sort(parents, by=x->x[1], rev=true)[1]
+            parent = next[2]
+            child = next[3]
+
+            new_uac = parent.uac * child.uac
+            new_bus = merge(components[parent.uac], components[child.uac], new_uac)
+            components[new_uac] = new_bus
+
+            new_node = merge(parent, child, new_uac)
+            nodes[new_uac] = new_node
+            update_nodes!(nodes, parent.uac, child.uac, new_node)
+            delete!(nodes, parent.uac)
+            delete!(nodes, child.uac)
+
+            continue
+        end
+
+        # if we got here, something went wrong and would result in an endless loop
+        @error("Could not resolve merging of busses")
+        break
+    end
+
+    return components[first(values(nodes)).uac]
+end
--- test/initialization/bus_merge.jl
+++ test/initialization/bus_merge.jl
@@ -707,3 +707,248 @@ end
 @testset "merge_busses_cross_shape_full_manual" begin
     test_merge_busses_cross_shape_full_manual()
 end
+
+function test_merge_busses_cross_shape_full_automatic()
+    components_config = energy_system_complicated()
+    simulation_parameters = Dict{String,Any}(
+        "time_step_seconds" => 900,
+        "time" => 0,
+        "epsilon" => 1e-9
+    )
+    components = Resie.load_components(components_config, simulation_parameters)
+    busses = Grouping()
+    for (key,val) in pairs(components)
+        if val.sys_function == EnergySystems.sf_bus
+            busses[key] = val
+        end
+    end
+    new_bus = EnergySystems.merge_busses(busses)
+
+    expected = [
+        "TST_SRC_04",
+        "TST_SRC_05",
+        "TST_SRC_03",
+        "TST_SRC_02",
+        "TST_BFT_02",
+        "TST_SRC_01",
+        "TST_BFT_01",
+        "TST_BFT_03",
+        "TST_BFT_05",
+        "TST_BFT_04",
+    ]
+    inputs = [f.source.uac for f in new_bus.input_interfaces]
+    @test inputs == expected
+    @test new_bus.connectivity.input_order == expected
+
+    expected = [
+        "TST_DEM_02",
+        "TST_BFT_02",
+        "TST_DEM_01",
+        "TST_BFT_01",
+        "TST_DEM_03",
+        "TST_BFT_03",
+        "TST_DEM_04",
+        "TST_BFT_04",
+        "TST_DEM_05",
+        "TST_BFT_05",
+    ]
+    outputs = [f.target.uac for f in new_bus.output_interfaces]
+    @test outputs == expected
+    @test new_bus.connectivity.output_order == expected
+
+    expected = [
+        ("TST_SRC_04",1),
+        ("TST_SRC_05",2),
+        ("TST_SRC_03",3),
+        ("TST_SRC_02",4),
+        ("TST_BFT_02",5),
+        ("TST_SRC_01",6),
+        ("TST_BFT_01",7),
+        ("TST_BFT_03",8),
+        ("TST_BFT_05",9),
+        ("TST_BFT_04",10),
+    ]
+    inputs = [
+        (row.source.uac, row.priority)
+        for row in sort(collect(values(new_bus.balance_table_inputs)), by=x->x.priority)
+    ]
+    @test inputs == expected
+
+    expected = [
+        ("TST_DEM_02",1),
+        ("TST_BFT_02",2),
+        ("TST_DEM_01",3),
+        ("TST_BFT_01",4),
+        ("TST_DEM_03",5),
+        ("TST_BFT_03",6),
+        ("TST_DEM_04",7),
+        ("TST_BFT_04",8),
+        ("TST_DEM_05",9),
+        ("TST_BFT_05",10),
+    ]
+    outputs = [
+        (row.target.uac, row.priority)
+        for row in sort(collect(values(new_bus.balance_table_outputs)), by=x->x.priority)
+    ]
+    @test outputs == expected
+
+    expected = [
+        [0,0,0,0,0,0,1,1,0,0],
+        [0,0,0,0,0,0,0,0,1,1],
+        [0,0,0,0,1,1,1,1,1,1],
+        [1,1,0,0,1,1,1,1,1,1],
+        [1,0,0,0,0,0,0,0,0,0],
+        [0,0,1,1,1,1,1,1,1,1],
+        [0,0,1,0,0,0,0,0,0,0],
+        [0,0,0,0,1,0,1,1,1,1],
+        [0,0,0,0,0,0,0,0,1,0],
+        [0,0,0,0,0,0,1,0,0,0],
+    ]
+    @test new_bus.connectivity.energy_flow == expected
+end
+
+@testset "merge_busses_cross_shape_full_automatic" begin
+    test_merge_busses_cross_shape_full_automatic()
+end
+
+function energy_system_busses_only()::Dict{String,Any}
+    return Dict{String,Any}(
+        "TST_BUS_01" => Dict{String,Any}(
+            "type" => "Bus",
+            "medium" => "m_h_w_ht1",
+            "control_refs" => [],
+            "connections" => Dict{String, Any}(
+                "input_order" => [],
+                "output_order" => [
+                    "TST_BUS_03"
+                ],
+            )
+        ),
+        "TST_BUS_02" => Dict{String,Any}(
+            "type" => "Bus",
+            "medium" => "m_h_w_ht1",
+            "control_refs" => [],
+            "connections" => Dict{String, Any}(
+                "input_order" => [],
+                "output_order" => [
+                    "TST_BUS_04"
+                ],
+            )
+        ),
+        "TST_BUS_03" => Dict{String,Any}(
+            "type" => "Bus",
+            "medium" => "m_h_w_ht1",
+            "control_refs" => [],
+            "connections" => Dict{String, Any}(
+                "input_order" => [
+                    "TST_BUS_01",
+                ],
+                "output_order" => [
+                    "TST_BUS_04",
+                ],
+                "energy_flow" => [
+                    [1],
+                ]
+            )
+        ),
+        "TST_BUS_04" => Dict{String,Any}(
+            "type" => "Bus",
+            "medium" => "m_h_w_ht1",
+            "control_refs" => [],
+            "connections" => Dict{String, Any}(
+                "input_order" => [
+                    "TST_BUS_02",
+                    "TST_BUS_03",
+                ],
+                "output_order" => [
+                    "TST_BUS_05",
+                ],
+                "energy_flow" => [
+                    [1],
+                    [1],
+                ]
+            )
+        ),
+        "TST_BUS_05" => Dict{String,Any}(
+            "type" => "Bus",
+            "medium" => "m_h_w_ht1",
+            "control_refs" => [],
+            "connections" => Dict{String, Any}(
+                "input_order" => [
+                    "TST_BUS_04",
+                ],
+                "output_order" => [
+                    "TST_BUS_06",
+                    "TST_BUS_07",
+                ],
+                "energy_flow" => [
+                    [1,1],
+                ]
+            )
+        ),
+        "TST_BUS_06" => Dict{String,Any}(
+            "type" => "Bus",
+            "medium" => "m_h_w_ht1",
+            "control_refs" => [],
+            "connections" => Dict{String, Any}(
+                "input_order" => [
+                    "TST_BUS_05",
+                ],
+                "output_order" => [
+                    "TST_BUS_08",
+                ],
+                "energy_flow" => [
+                    [1],
+                ]
+            )
+        ),
+        "TST_BUS_07" => Dict{String,Any}(
+            "type" => "Bus",
+            "medium" => "m_h_w_ht1",
+            "control_refs" => [],
+            "connections" => Dict{String, Any}(
+                "input_order" => [
+                    "TST_BUS_05",
+                ],
+                "output_order" => [],
+                "energy_flow" => [
+                    [1,1],
+                    [1,1],
+                    [1,0],
+                ]
+            )
+        ),
+        "TST_BUS_08" => Dict{String,Any}(
+            "type" => "Bus",
+            "medium" => "m_h_w_ht1",
+            "control_refs" => [],
+            "connections" => Dict{String, Any}(
+                "input_order" => [
+                    "TST_BUS_06",
+                ],
+                "output_order" => [],
+            )
+        ),
+    )
+end
+
+function test_merge_busses_order_of_merges()
+    components_config = energy_system_busses_only()
+    simulation_parameters = Dict{String,Any}(
+        "time_step_seconds" => 900,
+        "time" => 0,
+        "epsilon" => 1e-9
+    )
+    components = Resie.load_components(components_config, simulation_parameters)
+    chains = Resie.find_chains(values(components), EnergySystems.sf_bus)
+    chain = Grouping()
+    for component in chains[1]
+        chain[component.uac] = component
+    end
+    merged = EnergySystems.merge_busses(chain)
+    @test merged isa Bus
+end
+
+@testset "merge_busses_order_of_merges" begin
+    test_merge_busses_order_of_merges()
+end
