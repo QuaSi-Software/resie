8bd7f4b6487f0d64db19f50f81a92564c1044133
Etienne Ott <etienne.ott@siz-energieplus.de>
2024-02-21 11:40:46 +0100

    Fix failing loading tests due to testing an incomplete energy system.

--- test/initialization/loading.jl
+++ test/initialization/loading.jl
@@ -5,20 +5,24 @@ using Resie.EnergySystems
 
 function test_load_from_dict()
     components_config = Dict{String,Any}(
-        "TST_BT_01" => Dict{String,Any}(
-            "type" => "BufferTank",
+        "TST_GRI_01" => Dict{String,Any}(
+            "type" => "GridConnection",
             "control_refs" => [],
-            "output_refs" => [],
-            "capacity" => 40000,
-            "load" => 20000,
-            "strategy" => Dict{String,Any}(
-                "name" => "Default"
-            )
+            "output_refs" => ["TST_HP_01"],
+            "is_source" => true,
+            "medium" => "m_h_w_lt1",
+        ),
+        "TST_GRI_02" => Dict{String,Any}(
+            "type" => "GridConnection",
+            "control_refs" => [],
+            "output_refs" => ["TST_HP_01"],
+            "is_source" => true,
+            "medium" => "m_e_ac_230v",
         ),
         "TST_HP_01" => Dict{String,Any}(
             "type" => "HeatPump",
             "control_refs" => ["TST_BT_01"],
-            "output_refs" => [],
+            "output_refs" => ["TST_BT_01"],
             "strategy" => Dict{String,Any}(
                 "name" => "storage_driven",
                 "high_threshold" => 0.5,
@@ -27,6 +31,25 @@ function test_load_from_dict()
             "power_th" => 20000,
             "constant_cop" => 3.0
         ),
+        "TST_BT_01" => Dict{String,Any}(
+            "type" => "BufferTank",
+            "control_refs" => [],
+            "output_refs" => ["TST_DEM_01"],
+            "medium" => "m_h_w_ht1",
+            "capacity" => 40000,
+            "load" => 20000,
+            "strategy" => Dict{String,Any}(
+                "name" => "Default"
+            )
+        ),
+        "TST_DEM_01" => Dict{String,Any}(
+            "type" => "Demand",
+            "control_refs" => [],
+            "output_refs" => [],
+            "medium" => "m_h_w_ht1",
+            "constant_demand" => 5000,
+            "constant_temperature" => 60,
+        ),
     )
     simulation_params = Dict{String,Any}(
         "time" => 0,
@@ -34,7 +57,7 @@ function test_load_from_dict()
         "epsilon" => 1e-9
     )
     components = Resie.load_components(components_config, simulation_params)
-    @test length(keys(components)) == 2
+    @test length(keys(components)) == 5
     @test typeof(components["TST_BT_01"]) == Resie.EnergySystems.BufferTank
     @test components["TST_BT_01"].sys_function == Resie.EnergySystems.sf_storage
     @test components["TST_HP_01"].power_th == 20000
@@ -46,10 +69,28 @@ end
 
 function test_load_custom_medium_categories()
     components_config = Dict{String,Any}(
+        "TST_GRI_01" => Dict{String,Any}(
+            "type" => "GridConnection",
+            "control_refs" => [],
+            "output_refs" => ["TST_ELY_01"],
+            "is_source" => true,
+            "medium" => "m_e_dc_1000v",
+        ),
+        "TST_GRI_02" => Dict{String,Any}(
+            "type" => "GridConnection",
+            "control_refs" => [],
+            "output_refs" => ["TST_HP_01"],
+            "is_source" => true,
+            "medium" => "m_e_dc_1000v",
+        ),
         "TST_ELY_01" => Dict{String,Any}(
             "type" => "Electrolyser",
             "control_refs" => [],
-            "output_refs" => ["TST_HP_01"],
+            "output_refs" => [
+                "TST_HP_01",
+                "TST_GRO_02",
+                "TST_GRO_03"
+            ],
             "power_el" => 1000,
             "m_el_in" => "m_e_dc_1000v",
             "m_heat_out" => "m_h_w_55c",
@@ -62,7 +103,7 @@ function test_load_custom_medium_categories()
         "TST_HP_01" => Dict{String,Any}(
             "type" => "HeatPump",
             "control_refs" => [],
-            "output_refs" => [],
+            "output_refs" => ["TST_GRO_01"],
             "strategy" => Dict{String,Any}(
                 "name" => "demand_driven",
             ),
@@ -71,7 +112,28 @@ function test_load_custom_medium_categories()
             "m_heat_out" => "m_h_w_85c",
             "power_th" => 12000,
             "constant_cop" => 3.0
-        )
+        ),
+        "TST_GRO_01" => Dict{String,Any}(
+            "type" => "GridConnection",
+            "control_refs" => [],
+            "output_refs" => [],
+            "is_source" => false,
+            "medium" => "m_h_w_85c",
+        ),
+        "TST_GRO_02" => Dict{String,Any}(
+            "type" => "GridConnection",
+            "control_refs" => [],
+            "output_refs" => [],
+            "is_source" => false,
+            "medium" => "m_c_g_h2-pure",
+        ),
+        "TST_GRO_03" => Dict{String,Any}(
+            "type" => "GridConnection",
+            "control_refs" => [],
+            "output_refs" => [],
+            "is_source" => false,
+            "medium" => "m_c_g_o2-impure",
+        ),
     )
     simulation_params = Dict{String,Any}(
         "time" => 0,
