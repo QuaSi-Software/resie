fddba9b67c7266e3ba09bdefee129741193267b4
Etienne Ott <etienne.ott@siz-energieplus.de>
2024-03-18 10:44:49 +0100

    Update setup of logging both in tests execution and simulation CLI.

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,18 +3,20 @@ using Test
 include("../src/resie_logger.jl")
 using .Resie_Logger
 
-# set up Resie_Logger
-log_to_console = false                                       # flag if logs should be printed to console
-log_to_file = false                                          # flag if logs should be written to a file
-general_logfile_path = "output/logfile_general.log"          # general log file path for debug, info, warn, error
-balanceWarn_logfile_path = "output/logfile_balanceWarn.log"  # balanceWarn log file path, only for balanceWarning
-min_log_level = Resie_Logger.Logging.Debug                   # the minimal log level for the output. Can be one of Debug, Info, Warn, Error or
-                                                             # a custom LogLevel, like Logging.LogLevel(500) for BalanceWarning
-log_file_general, log_file_balanceWarn = Resie_Logger.start_logger(log_to_console,
-                                                                   log_to_file,
-                                                                   general_logfile_path,
-                                                                   balanceWarn_logfile_path,
-                                                                   min_log_level)
+# set up logging
+log_to_console = false
+log_to_file = false
+general_logfile_path = "output/logfile_general.log"
+balanceWarn_logfile_path = "output/logfile_balanceWarn.log"
+min_log_level = Resie_Logger.Logging.Debug
+log_file_general, log_file_balanceWarn = Resie_Logger.start_logger(
+    log_to_console,
+    log_to_file,
+    general_logfile_path,
+    balanceWarn_logfile_path,
+    min_log_level
+)
+
 @testset "tests_are_working" begin
     @test true
 end
