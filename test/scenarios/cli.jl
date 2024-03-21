dc1241a28eaa36e64e9ab484b7a03c9799ca981f
Etienne Ott <etienne.ott@siz-energieplus.de>
2024-03-21 10:26:05 +0100

    Restructure scenario tool to use a CLI structure with multiple commands possible.

--- test/scenarios/cli.jl
+++ test/scenarios/cli.jl
@@ -1,19 +1,29 @@
 """
-Utility script generate the output from predefined scenarios for testing purposes. The
-script can be used like so:
-    `julia --project=. ./test/scenarios/generate_output.jl "multisector_district"`
+Utility script to generate the output from predefined scenarios for testing purposes.
+
+The script can be used like so:
+    `julia --project=. ./test/scenarios/cli.jl generate_output multisector_district`
 
 This generates the output for the scenario `multisector_district`. It is assumed that the
 script is called from the directory two levels above this script, which is the main
 directory of ReSiE.
 
-If no argument is given, generates the output for all scenarios. Be aware that this might
-take a while.
+Several commands exist for the script, which are:
+    * `generate_output`: Generate the output for a scenario by running the simulation
+
+Each of the commands can be given, as second argument, the name of a scenario.
+
+If no scenario name is given, performs the command for all scenarios. Be aware that this
+might take a while.
 """
 
 include("../../src/resie_logger.jl")
 using .Resie_Logger
 using Resie
+
+KNOWN_COMMANDS = Set([
+    "generate_output"
+])
 
 """
     setup_logger(subdir)
@@ -47,19 +57,125 @@ function setup_logger(subdir)
 end
 
 """
+    generate_output(name, subdir)
+
+Generate the output for the given scenario.
+
+# Arguments
+-`name::String`: The name of the scenario
+-`subdir::String`: Full path of the subdir for the scenario
+"""
+function generate_output(name::String, subdir::String)
+    println("Generating scenario $name in dir $subdir")
+    println("|Logr+|RdJsn|SmPrm|LdCmp|OrdOp|RnSim|Logr-|")
+
+    log_file_general, log_file_balanceWarn = setup_logger(subdir)
+    print("|  ✓  ")
+
+    project_config = nothing
+    try
+        project_config = Resie.read_JSON(joinpath(subdir, "inputs.json"))
+        print("|  ✓  ")
+    catch error
+        @error error
+        print("|  X  ")
+    end
+
+    sim_params = nothing
+    try
+        if project_config !== nothing
+            sim_params = Resie.get_simulation_params(project_config)
+            print("|  ✓  ")
+        else
+            print("|     ")
+        end
+    catch error
+        @error error
+        print("|  X  ")
+    end
+
+    components = nothing
+    try
+        if sim_params !== nothing && project_config !== nothing
+            components = Resie.load_components(
+                project_config["components"],
+                sim_params
+            )
+            print("|  ✓  ")
+        else
+            print("|     ")
+        end
+    catch error
+        @error error
+        print("|  X  ")
+    end
+
+    step_order = nothing
+    try
+        if project_config !== nothing && components !== nothing
+            if (
+                haskey(project_config, "order_of_operation")
+                && length(project_config["order_of_operation"]) > 0
+            )
+                step_order = Resie.load_order_of_operations(
+                    project_config["order_of_operation"],
+                    components
+                )
+            else
+                step_order = Resie.calculate_order_of_operations(components)
+            end
+            print("|  ✓  ")
+        else
+            print("|     ")
+        end
+    catch error
+        @error error
+        print("|  X  ")
+    end
+
+    if (
+        project_config !== nothing
+        && sim_params !== nothing
+        && components !== nothing
+        && step_order !== nothing
+    )
+        Resie.run_simulation_loop(project_config, sim_params, components, step_order)
+        print("|  ✓  ")
+    else
+        print("|     ")
+    end
+
+    Resie_Logger.close_logger(log_file_general, log_file_balanceWarn)
+    print("|  ✓  ")
+
+    println("|")
+end
+
+"""
     main()
 
 Entry point for the script.
 
-Parses arguments and performs the functions as described in the documentation above.
+Parses arguments and performs the commands as described in the documentation above.
 """
 function main()
-    if length(ARGS) > 0 && ARGS[1] != ""
-        scenario_name = strip(ARGS[1])
-        println("Given scenario to generate: $scenario_name")
+    if length(ARGS) < 1
+        println("No command given.")
+        return
+    end
+
+    command = lowercase(ARGS[1])
+    if !(command in KNOWN_COMMANDS)
+        println("Unknown command.")
+        return
+    end
+
+    if length(ARGS) >= 2 && ARGS[2] != ""
+        scenario_name = ARGS[2]
+        println("Given scenario: $scenario_name")
     else
         scenario_name = nothing
-        println("No argument given - generating outputs for all scenarios")
+        println("No scenario given - performing command for all scenarios")
     end
 
     scenarios_dir = joinpath(".", "test", "scenarios")
@@ -73,89 +189,9 @@ function main()
             continue
         end
 
-        println("Generating scenario $name in dir $subdir")
-        println("|Logr+|RdJsn|SmPrm|LdCmp|OrdOp|RnSim|Logr-|")
-
-        log_file_general, log_file_balanceWarn = setup_logger(subdir)
-        print("|  ✓  ")
-
-        project_config = nothing
-        try
-            project_config = Resie.read_JSON(joinpath(subdir, "inputs.json"))
-            print("|  ✓  ")
-        catch error
-            @error error
-            print("|  X  ")
+        if command == "generate_output"
+            generate_output(name, subdir)
         end
-
-        sim_params = nothing
-        try
-            if project_config !== nothing
-                sim_params = Resie.get_simulation_params(project_config)
-                print("|  ✓  ")
-            else
-                print("|     ")
-            end
-        catch error
-            @error error
-            print("|  X  ")
-        end
-
-        components = nothing
-        try
-            if sim_params !== nothing && project_config !== nothing
-                components = Resie.load_components(
-                    project_config["components"],
-                    sim_params
-                )
-                print("|  ✓  ")
-            else
-                print("|     ")
-            end
-        catch error
-            @error error
-            print("|  X  ")
-        end
-
-        step_order = nothing
-        try
-            if project_config !== nothing && components !== nothing
-                if (
-                    haskey(project_config, "order_of_operation")
-                    && length(project_config["order_of_operation"]) > 0
-                )
-                    step_order = Resie.load_order_of_operations(
-                        project_config["order_of_operation"],
-                        components
-                    )
-                else
-                    step_order = Resie.calculate_order_of_operations(components)
-                end
-                print("|  ✓  ")
-            else
-                print("|     ")
-            end
-        catch error
-            @error error
-            print("|  X  ")
-        end
-
-        if (
-            project_config !== nothing
-            && sim_params !== nothing
-            && components !== nothing
-            && step_order !== nothing
-        )
-            Resie.run_simulation_loop(project_config, sim_params, components, step_order)
-            print("|  ✓  ")
-        else
-            print("|     ")
-        end
-
-        Resie_Logger.close_logger(log_file_general, log_file_balanceWarn)
-        print("|  ✓  ")
-
-        println("|")
     end
 end
 
