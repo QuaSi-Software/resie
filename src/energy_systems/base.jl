f55071e327a9b244c50f75a3330f8cf14f3a0a09
edemartin-siz <elias.demartin@siz-energieplus.de>
2025-07-31 10:19:44 +0200

    InputError now defined in Resie.jl

--- src/energy_systems/base.jl
+++ src/energy_systems/base.jl
@@ -243,12 +243,6 @@ Base.@kwdef mutable struct SystemInterface
 end
 
 """
-Custom error handler for exception "InputError".
-Call with throw(InputError)
-"""
-struct InputError <: Exception end
-
-"""
     set_storage_transfer!(interface, value)
 
 Sets the flag to decide over storage potential transfer to the given boolean value. Note
