7b624cbdb5194500bb0aa06f629624df08c33b1b
edemartin-siz <elias.demartin@siz-energieplus.de>
2025-12-19 15:59:16 +0100

    quick fix for logger error

--- src/resie_logger.jl
+++ src/resie_logger.jl
@@ -54,24 +54,24 @@ To add further custom levels, you need to define a new constant using the
 CustomLevel struct, the corresponding macro and export the macro. 
 Also define the behaviour in Base.show() and Logging.handle_message() below.
 """
-const BalanceWarning = CustomLevel(500, "BalanceWarning")
-# const BalanceWarning = LogLevel(500)
+# const BalanceWarning = CustomLevel(500, "BalanceWarning")
+const BalanceWarning = LogLevel(500)
 macro balanceWarn(exprs...)
     quote
         @logmsg BalanceWarning $(map(x -> esc(x), exprs)...)
     end
 end
 
-# define basic functions to handle the CustomLevels
-Base.isless(a::CustomLevel, b::LogLevel) = isless(a.level, b.level)
-Base.isless(a::LogLevel, b::CustomLevel) = isless(a.level, b.level)
-Base.convert(::Type{LogLevel}, level::CustomLevel) = LogLevel(level.level)
-Base.show(io::IO, level::CustomLevel) =
-    if level == BalanceWarning
-        print(io, "BalanceWarning")
-    else
-        show(io, LogLevel(level))
-    end
+# # define basic functions to handle the CustomLevels
+# Base.isless(a::CustomLevel, b::LogLevel) = isless(a.level, b.level)
+# Base.isless(a::LogLevel, b::CustomLevel) = isless(a.level, b.level)
+# Base.convert(::Type{LogLevel}, level::CustomLevel) = LogLevel(level.level)
+# Base.show(io::IO, level::CustomLevel) =
+#     if level == BalanceWarning
+#         print(io, "BalanceWarning")
+#     else
+#         show(io, LogLevel(level))
+#     end
 
 """
 handle CustomLogger and CustomLevel
