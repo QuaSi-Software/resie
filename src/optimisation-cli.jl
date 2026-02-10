905b012c8cb49fa0c00f3246940c10030262dca0
Etienne Ott <etienne.ott@siz-energieplus.de>
2026-02-10 15:50:51 +0100

    Implement combined Monte-Carlo-Annealing method for optimisation.

--- src/optimisation-cli.jl
+++ src/optimisation-cli.jl
@@ -9,7 +9,8 @@ const BOUNDS = Dict{String,Tuple}(
     "TST_BAT_01" => ("capacity", 0, 30000),
     "TST_BFT_01" => ("capacity", 0, 60000),
 )
-const NR_TRIES = 100
+const NBH_SCALE = 0.5
+const NR_TRIES = 200
 const OPEX_YEARS = 20
 const HEAT_DEMAND = 12000000
 const POWER_DEMAND = 5000000
@@ -60,17 +61,54 @@ function main()
     min_lcc = Inf
     min_emissions = Inf
 
-    # simple monte carlo
-    for idx in range(0, NR_TRIES)
-        # set parameters to equally distributed random values
-        hp_power = rand(BOUNDS["TST_HP_01"][2]:BOUNDS["TST_HP_01"][3])
-        pv_power = rand(BOUNDS["TST_PV_01"][2]:BOUNDS["TST_PV_01"][3])
-        bat_cap = rand(BOUNDS["TST_BAT_01"][2]:BOUNDS["TST_BAT_01"][3])
-        bt_cap = rand(BOUNDS["TST_BFT_01"][2]:BOUNDS["TST_BFT_01"][3])
-        inputs["components"]["TST_HP_01"]["power_th"] = hp_power
-        inputs["components"]["TST_PV_01"]["scale"] = pv_power
-        inputs["components"]["TST_BAT_01"]["capacity"] = bat_cap
-        inputs["components"]["TST_BFT_01"]["capacity"] = bt_cap
+    # combined monte carlo and simulated annealing
+    # the temperature determines if a completely random or existing sample is used as starting
+    # point, determines the size of the neighborhood and the number of results (sorted by)
+    # global measure, from which a new sample is drawn
+    for idx in range(1, NR_TRIES)
+        # temperature schedule is simple inverse logistic curve
+        temperature = 1.0 - 1.0 / (1.0 + exp(-8.0 * (idx / NR_TRIES - 0.5)))
+
+        if length(all_results) == 0 || rand() < temperature
+            # set parameters to equally distributed random values across whole parameter space
+            hp_power = rand(BOUNDS["TST_HP_01"][2]:BOUNDS["TST_HP_01"][3])
+            pv_power = rand(BOUNDS["TST_PV_01"][2]:BOUNDS["TST_PV_01"][3])
+            bat_cap = rand(BOUNDS["TST_BAT_01"][2]:BOUNDS["TST_BAT_01"][3])
+            bt_cap = rand(BOUNDS["TST_BFT_01"][2]:BOUNDS["TST_BFT_01"][3])
+            inputs["components"]["TST_HP_01"]["power_th"] = hp_power
+            inputs["components"]["TST_PV_01"]["scale"] = pv_power
+            inputs["components"]["TST_BAT_01"]["capacity"] = bat_cap
+            inputs["components"]["TST_BFT_01"]["capacity"] = bt_cap
+            print(". ")
+        else
+            # set parameters to neighborhood of existing result, drawn from the top results
+            # by global measure, where temperature determines the results pool and size of
+            # neighborhood
+            sample_idx = rand(1:max(1, Int(round(length(all_results) * temperature))))
+            print("$sample_idx ")
+            sample = sample_idx >= 1 && sample_idx <= length(all_results) ? all_results[sample_idx] : all_results[1]
+
+            range = NBH_SCALE * temperature * (BOUNDS["TST_HP_01"][3] - BOUNDS["TST_HP_01"][2])
+            hp_power = sample[4] + rand((-0.5 * range):(0.5 * range))
+            hp_power = max(BOUNDS["TST_HP_01"][2], min(BOUNDS["TST_HP_01"][3], hp_power))
+
+            range = NBH_SCALE * temperature * (BOUNDS["TST_PV_01"][3] - BOUNDS["TST_PV_01"][2])
+            pv_power = sample[5] + rand((-0.5 * range):(0.5 * range))
+            pv_power = max(BOUNDS["TST_PV_01"][2], min(BOUNDS["TST_PV_01"][3], pv_power))
+
+            range = NBH_SCALE * temperature * (BOUNDS["TST_BAT_01"][3] - BOUNDS["TST_BAT_01"][2])
+            bat_cap = sample[6] + rand((-0.5 * range):(0.5 * range))
+            bat_cap = max(BOUNDS["TST_BAT_01"][2], min(BOUNDS["TST_BAT_01"][3], bat_cap))
+
+            range = NBH_SCALE * temperature * (BOUNDS["TST_BFT_01"][3] - BOUNDS["TST_BFT_01"][2])
+            bt_cap = sample[7] + rand((-0.5 * range):(0.5 * range))
+            bt_cap = max(BOUNDS["TST_BFT_01"][2], min(BOUNDS["TST_BFT_01"][3], bt_cap))
+
+            inputs["components"]["TST_HP_01"]["power_th"] = hp_power
+            inputs["components"]["TST_PV_01"]["scale"] = pv_power
+            inputs["components"]["TST_BAT_01"]["capacity"] = bat_cap
+            inputs["components"]["TST_BFT_01"]["capacity"] = bt_cap
+        end
 
         # run sim and record results
         run_results = run_simulation(inputs)
@@ -79,24 +117,25 @@ function main()
         min_lcc = obj_results.lcc < min_lcc ? obj_results.lcc : min_lcc
         min_emissions = obj_results.emissions < min_emissions ? obj_results.emissions : min_emissions
 
-        if idx % Int(NR_TRIES / 20) == 0
-            print("$idx ")
+        # calculate global measure and sort by it
+        for res in all_results
+            res[1] = sqrt(((res[2]) / min_lcc - 1.0)^2 + (res[3] / min_emissions - 1.0)^2)
+        end
+        sort!(all_results; by=x -> x[1])
+
+        if idx % 5 == 0
+            print("| ")
         end
     end
     print("\nFinished simulation, now plotting\n")
-
-    # calculate global measure and sort by it
-    for res in all_results
-        res[1] = sqrt(((res[2]) / min_lcc - 1.0)^2 + (res[3] / min_emissions - 1.0)^2)
-    end
-    sort!(all_results; by=x->x[1])
 
     # create scatter plot with objective function values
     x_values = [result[2] for result in all_results]
     y_values = [result[3] for result in all_results]
 
     scatter(x_values, y_values; xlabel="Life-cycle cost (capex + $OPEX_YEARS*opex) [€]",
-            ylabel="Operational emissions [kg/a]", title="Pareto Front", legend=false)
+            ylabel="Operational emissions [kg/a]", title="Pareto Front", legend=false,
+            xlims=(4e4, 9e4), ylims=(0, 3500))
     savefig("./output/pareto_front.html")
 
     println("Plotting finished, now finding Pareto-optimal points")
